1c1bfb0746617d41a5b060471b210cd4203ebcae
Ustym Ukhman <ustym.ukhman@gmail.com>
2026-08-26 20:08:04 +0300

    Add RoadRoller Minification

--- types/reflect/reflect.d.ts
+++ types/reflect/reflect.d.ts
@@ -2,7 +2,7 @@
  * @author Brendan Duncan / https://github.com/brendan-duncan
  */
 import { Type, Struct, Alias, type Node, Function, /* VariableExpr, CreateExpr,
-    Let, CallExpr, Call, */ Expression, type Argument, type Member, type Attribute } from "../wgsl_ast.js";
+    Let, Const, */ type Expression, /* CallExpr, Call, */ type Argument, type Member, type Attribute } from "../wgsl_ast.js";
 import { FunctionInfo, VariableInfo, AliasInfo, StructInfo, TypeInfo, MemberInfo, /* OutputInfo, */ InputInfo, EntryFunctions } from "./info.js";
 declare class _FunctionResources {
     node: Function;
